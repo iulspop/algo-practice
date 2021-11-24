1f2388ebd9bfbf09ff19cf6407763d25c6352a2e
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-11-24 11:06:38 -0500

    Configure type for schedule max jobs

--- problem-solutions/schedule_max_jobs/schedule_max_jobs.go
+++ problem-solutions/schedule_max_jobs/schedule_max_jobs.go
@@ -1,5 +1,30 @@
-package main
+package schedule_max_jobs
 
-func ScheduleMaxJobs(x, y int) (res int) {
-  return x + y
+func ScheduleMaxJobs(jobs [][2]int) (res [][2]int) {
+  return jobs
 }
+
+/*
+
+Input: [[0, 10], [5, 10]] // set of projects with start and end times
+Output: [[]...] // largest possible set of non-overlapping projects.
+
+Approach: Full Verification through recursive branch check
+
+Algo:
+- Create array of possible job sets
+- Create array for temp job set
+
+call create possible job sets(all jobs array)
+  - If no more jobs
+      - add job set to possible job sets
+      - reset job set to empty
+  - Pick the project that is most recent
+      - add to temp job set
+      - call create possible job sets without it
+
+- array with all possible job sets
+  - map/reduce to number of jobs
+  - select first job set with maximum number
+
+*/
--- problem-solutions/schedule_max_jobs/schedule_max_jobs_test.go
+++ problem-solutions/schedule_max_jobs/schedule_max_jobs_test.go
@@ -1,37 +1,18 @@
-package main
+package schedule_max_jobs
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestScheduleMaxJobs(t *testing.T) {
-  got := ScheduleMaxJobs(4, 5)
-  want := 9
+  got := ScheduleMaxJobs([][2]int{{1, 2},{1, 2}})
+  want := [][2]int{{1, 2},{1, 2}}
 
-  if got != want {
+  if !reflect.DeepEqual(got, want) {
     t.Errorf("got %q, wanted %q", got, want)
+  } else {
+    fmt.Printf("got %q, wanted %q\n", got, want)
   }
 }
-
-/*
-
-Input: [[0, 10], [5, 10]] // set of projects with start and end times
-Output: [[]...] // largest possible set of non-overlapping projects.
-
-Approach: Full Verification through recursive branch check
-
-Algo:
-- Create array of possible job sets
-- Create array for temp job set
-
-call create possible job sets(all jobs array)
-  - If no more jobs
-      - add job set to possible job sets
-      - reset job set to empty
-  - Pick the project that is most recent
-      - add to temp job set
-      - call create possible job sets without it
-
-- array with all possible job sets
-  - map/reduce to number of jobs
-  - select first job set with maximum number
-
-*/

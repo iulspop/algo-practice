8db6117ab6189a5143a313db3dc9c84b3d06c408
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-12-06 08:25:24 -0500

    Move Go exercises to seperate repo

--- problems/math/math.go
+++
@@ -1,9 +0,0 @@
-package math
-
-func Add(x, y int) (res int) {
-	return x + y
-}
-
-func Subtract(x, y int) (res int) {
-	return x - y
-}
--- problems/movie-scheduling.test.js
+++
@@ -1,9 +0,0 @@
-/*
-
-Input: [[0, 10], [5, 10]] // set of projects with start and end times
-Output: [...] // largest possible set of non-overlapping projects.
-
-
-*/
-
-
